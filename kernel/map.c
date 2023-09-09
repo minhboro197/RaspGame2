93439a29bab5d06465553b5778e1aff0620186e7
nepanhdoan <s3880604@rmit.edu.vn>
2023-09-09 22:04:23 +0700

    test git

--- kernel/map.c
+++ kernel/map.c
@@ -13,6 +13,7 @@ void draw_map(){
             drawGameAsset(0, 986 - SCREEN_WIDTH_OFFSET,i*48,38,46,wall_block_allArray);
             //drawRectARGB32(0,i*40,40,40,0x00AA0000,1);
             //drawRectARGB32(986,i*40,40,40,0x00AA0000,1);
+            //helloo
         }
         drawGameAsset(0, j*40 + SCREEN_WIDTH_OFFSET,0,38,46,wall_block_allArray);
         drawGameAsset(0, j*40 + SCREEN_WIDTH_OFFSET,720,38,46,wall_block_allArray);
